ada642bb7647857eea5c39e833333aeb9a663dde
JeannieM <bean.mccarthy@gmail.com>
2020-02-11 11:23:42 -0500

    added club class

--- src/Club.java
+++ src/Club.java
@@ -1,4 +1,34 @@
-
+/**
+ * @author JeannieMcCarthy&RanaSiroosian
+ *
+ */
 public class Club {
-
+	
+	protected String name;
+	protected String address;
+	
+	public Club(String name, String address) {
+		super();
+		this.name = name;
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return String.format("Club [name=%s, address=%s]", name, address);
+	}
+	
+	
+	
 }
