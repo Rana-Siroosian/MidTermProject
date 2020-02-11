47c8974f1bacdb09712f6f314ff021a41262f654
Rana-Siroosian <rana.siroosian@gmail.com>
2020-02-11 11:01:54 -0500

    Here we are

---
+++ src/Member.java
@@ -0,0 +1,40 @@
+
+public class Member {
+
+	protected int id;
+	protected String name;
+	
+	
+	public Member() {
+		
+	}
+	
+	public Member (int id, String name) {
+		
+		this.id = id;
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%-10d%-15s", getId(), getName());
+	}
+	
+	
+}
