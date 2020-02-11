a8e0122468ce8b5072ffe30d82a6a10ffbe6bf4c
Rana-Siroosian <rana.siroosian@gmail.com>
2020-02-11 14:27:08 -0500

    Finally

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -8,62 +8,11 @@ import java.awt.image.BufferedImage;
  *@author RanaSiroosian&JeannieMcCarthy
  *
  */
-public class MidTerm {
+public class Midterm {
 
-	public static void main(String[] args) {
-		int width = 80;
-		int height = 18;
-		BufferedImage bufferedImage = new BufferedImage(
-				  width, height, 
-				  BufferedImage.TYPE_INT_RGB);
-				Graphics graphics = bufferedImage.getGraphics();
-				Graphics2D graphics2D = (Graphics2D) graphics;
-				graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, 
-				  RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-				graphics2D.drawString("FITTER", 12, 24);
-				for (int y = 0; y < height; y++) {
-				    StringBuilder stringBuilder = new StringBuilder();
-				 
-				    for (int x = 0; x < width; x++) {
-				        stringBuilder.append("*");
-				    }
-				 
-				    if (stringBuilder.toString().trim().isEmpty()) {
-				        continue;
-				    }
-				 
-				    System.out.println(stringBuilder);
-				}
-				
-				
-				
-				
-				for (int y = 0; y <height; y++) {
-				    StringBuilder stringBuilder = new StringBuilder();
-				 
-				    for (int x = 0; x < width; x++) {
-				        stringBuilder.append(bufferedImage.getRGB(x, y) == -16777216 ? "*" : " ");
-				    }
-				 
-				    if (stringBuilder.toString().trim().isEmpty()) {
-				        continue;
-				    }
-				 
-				    System.out.println(stringBuilder);
-				}
-				for (int y = 0; y < height; y++) {
-				    StringBuilder stringBuilder = new StringBuilder();
-				 
-				    for (int x = 0; x < width; x++) {
-				        stringBuilder.append(bufferedImage.getRGB(x, y) == -16777216 ? " " : "*");
-				    }
-				 
-				    if (stringBuilder.toString().trim().isEmpty()) {
-				        continue;
-				    }
-				 
-				    System.out.println(stringBuilder);
-				}
+	public static void main(String[] args) throws Exception {
+		
+		ClubArt.displayClubName("R A N A");
 	}
 
 }
